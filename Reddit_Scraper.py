5abf889f0ea3510057037cf7635a9ec34ac6f64f
WelsonM <91895980+WelsonM@users.noreply.github.com>
2023-03-09 15:58:58 -0500

    Add files via upload

--- Reddit_Scraper.py
+++ Reddit_Scraper.py
@@ -1,4 +1,5 @@
 import praw
+import pandas as pd
 
 # Read-only instance
 reddit_read_only = praw.Reddit(client_id="anJb3mWjKA1Qy3uz5FPUFg",         # your client id
@@ -6,13 +7,37 @@ reddit_read_only = praw.Reddit(client_id="anJb3mWjKA1Qy3uz5FPUFg",         # you
                                user_agent="491_Scraper")   # your user agent
 
 
-subreddit = reddit_read_only.subreddit("AskNetsec")
+subreddit = reddit_read_only.subreddit("datascience")
 
-# Display the name of the Subreddit
-print("Display Name:", subreddit.display_name)
+posts = subreddit.top("month")
+# Scraping the top posts of the current month
 
-# Display the title of the Subreddit
-print("Title:", subreddit.title)
+posts_dict = {"Title": [], "Post Text": [],
+              "ID": [], "Score": [],
+              "Total Comments": [], "Post URL": []
+              }
 
-# Display the description of the Subreddit
-print("Description:", subreddit.description)
+for post in posts:
+    # Title of each post
+    posts_dict["Title"].append(post.title)
+
+    # Text inside a post
+    posts_dict["Post Text"].append(post.selftext)
+
+    # Unique ID of each post
+    posts_dict["ID"].append(post.id)
+
+    # The score of a post
+    posts_dict["Score"].append(post.score)
+
+    # Total number of comments inside the post
+    posts_dict["Total Comments"].append(post.num_comments)
+
+    # URL of each post
+    posts_dict["Post URL"].append(post.url)
+
+# Saving the data in a pandas dataframe
+top_posts = pd.DataFrame(posts_dict)
+top_posts
+
+top_posts.to_csv("Top_Posts.csv", index=True)
